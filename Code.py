fbf26c510271249077fbec2ce4788b635c310ffa
Johanna <johanna.annau@gmail.com>
2021-01-18 11:54:35 +0100

    Some code

---
+++ Code.py
@@ -0,0 +1,10 @@
+#Some code goes here
+
+import json
+
+with open ('') as file:
+    real_people=load.json()
+
+    #use SelectString in CLI to only use data that you need 
+
+Select-String label 'person'
